b51882135f1ca9997b4476e46e2c448751acc5be
Frithjof Herb <frithjof.herb31@gmail.com>
2024-11-21 01:35:29 +1100

    Update install.bat to warn conda is required

--- install.bat
+++ install.bat
@@ -1,25 +1,31 @@
-@echo off
-call conda create -n ftirml -y
-echo Finished creating environment, now activating
-timeout /t 2 >nul
-call conda activate ftirml
-echo Environment activated, now installing python and spyder
-timeout /t 2 >nul
-call conda install python==3.10 spyder -y
-echo Finished installing python V3.10 and spyder, now installing pypi packages
-timeout /t 2 >nul
-call pip install -r pip_requirements.txt
-
-echo Finished installing pypi packages
-timeout /t 2 >nul
-
-
+set /p user_input="This script assumes you've installed conda. Do you have conda installed? (y/N): "
 set /p user_input="Install done. Do you want to launch Spyder? (Y/n): "
 if /i "%user_input%"=="n" (
     echo Exiting installer...
     exit /b
 ) else (
-    call spyder
+    @echo off
+    call conda create -n ftirml -y
+    echo Finished creating environment, now activating
+    timeout /t 2 >nul
+    call conda activate ftirml
+    echo Environment activated, now installing python and spyder
+    timeout /t 2 >nul
+    call conda install python==3.10 spyder -y
+    echo Finished installing python V3.10 and spyder, now installing pypi packages
+    timeout /t 2 >nul
+    call pip install -r pip_requirements.txt
+    
+    echo Finished installing pypi packages
+    timeout /t 2 >nul
+    set /p user_input="Install done. Do you want to launch Spyder? (Y/n): "
+    if /i "%user_input%"=="n" (
+        echo Exiting installer...
+        exit /b
+    ) else (
+        call spyder
+    )
+
 )
 
 pause
